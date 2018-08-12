58385359e6a43e8813f8433089e8c35e6a4e129e
Oomkar Pandit <oomkar@yahoo.com>
2018-08-12 15:03:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function destructivelyRemoveFirstKitten(){
 function appendKitten(name){
   console.log(kittens.length+'length of kittens')
   
-kittens=kittens.splice(end,0,name)
+kittens.splice(end,0,name)
 console.log(kittens.toString()+'items of kittens')
 console.log(newarray.toString()+'items of newarray')
 
