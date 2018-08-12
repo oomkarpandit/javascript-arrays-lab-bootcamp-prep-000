e9f69e5238f8881c4f080604b4519999d77fb8b2
Oomkar Pandit <oomkar@yahoo.com>
2018-08-12 14:48:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Add your functions and code here
+
+function destructivelyAppendKitten(name){
+  kittens.push(name)
+}
+function destructivelyPrependKitten(name){
+  kittens.unshift(name)
+}
+function destructivelyRemoveLastKitten(){
+  kittens.pop()
+}
+function destructivelyRemoveFirstKitten(){
+  kittens.shift()
+}
+function appendKitten(name){
+  kittens
+}
