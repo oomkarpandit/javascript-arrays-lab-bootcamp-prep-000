ef4d4c53031f7a9cd323ec85629ad343734a9ccf
Oomkar Pandit <oomkar@yahoo.com>
2018-08-12 15:01:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function appendKitten(name){
   var end=kittens.length
 var newarray=kittens.splice(end,0,name)
 console.log(kittens.toString()+'items of kittens')
-console.log(newarray.toString()+'items of kittens')
+console.log(newarray.toString()+'items of newarray')
 
 return newarray
 }
